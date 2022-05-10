09671c495b17cef3babd803828a3800ce6db3935
Aldo <aldo.vidales@gmail.com>
2022-05-10 14:13:38 -0500

    V1-app

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import {StyleSheet, Switch, View, Text} from 'react-native'
+export default class SwitchExample extends Component {
+state = {
+switchValue: false
+};
+render() {
+return (
+<View style={styles.container}>
+<Text style={styles.textStyle}>Pia Caja fuerte</Text>
+<Text style={styles.textStyle}>{this.state.switchValue ? '1' :'0'}</Text>
+<Switch
+value={this.state.switchValue} onValueChange
+={(switchValue)=>this.setState({switchValue})}
+style={styles.Switch}
+ios_backgroundColor="#fbfbfb"
+trackColor={{true: '#5AA8D5', false: 'grey'}}
+/>
+</View>
+);
+}
+}
+const styles = StyleSheet.create({ container:{
+flex:1,
+alignItems: 'center', justifyContent: 'center', backgroundColor: '#96f2ca',
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+},
+textStyle:{
+margin: 25,
+fontSize: 52, fontWeight: 'bold', textAlign: 'center', color: '#3a4a35'
+
+
+},
+Switch:{
+margin: 25,
+height: 120,
+
+transform: [{ rotate: '-90deg'}]
+
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+})
